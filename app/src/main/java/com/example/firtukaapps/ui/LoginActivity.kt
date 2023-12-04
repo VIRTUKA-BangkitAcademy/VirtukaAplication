da04db1d5f162714606d2944e30e1d1ab44c9fd4
nandanvsh <94284968+nandanvsh@users.noreply.github.com>
2023-12-04 19:58:03 +0700

    First Commit

---
+++ app/src/main/java/com/example/firtukaapps/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.firtukaapps.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firtukaapps.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/firtukaapps/ui/RegistrasiActivity.kt
@@ -0,0 +1,12 @@
+package com.example.firtukaapps.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firtukaapps.R
+
+class RegistrasiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrasi)
+    }
+}
